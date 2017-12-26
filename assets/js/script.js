b3756660ed07b0858107b675c808192a6613fbef
Gabriele Lippi <gabriele@lippi.net>
2017-12-26 12:35:42 +0100

    Second Exercise - duplicated cat instances, to be refactor in next lessons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
-var number = 0;
-var catImg = document.getElementById("cat");
-var counterDiv = document.getElementById("number");
-catImg.addEventListener(
+var number1 = 0;
+var number2 = 0;
+var catImg1 = document.getElementById("cat-1");
+var catImg2 = document.getElementById("cat-2");
+var counterDiv1 = document.getElementById("number-1");
+var counterDiv2 = document.getElementById("number-2");
+catImg1.addEventListener(
   "click",
   function() {
-    number++;
-    counterDiv.innerHTML = number;
+    number1++;
+    counterDiv1.innerHTML = number1;
+  },
+  false
+);
+catImg2.addEventListener(
+  "click",
+  function() {
+    number2++;
+    counterDiv2.innerHTML = number2;
   },
   false
 );
